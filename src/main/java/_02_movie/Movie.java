5c32c4198c2bc1c53717bf1e752f3bf44d2d4a15
jung <jungpark007@naver.com>
2021-03-14 01:41:20 +0900

    챕터 02 객체 지향 프로그래밍 - 협력에 관한 짧은 이야기

---
+++ src/main/java/_02_movie/Movie.java
@@ -0,0 +1,11 @@
+package _02_movie;
+
+public class Movie {
+    public Money getFee() {
+        return null;
+    }
+
+    public Money calculateMovieFee(Screening screening) {
+        return null;
+    }
+}
